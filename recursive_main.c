3e016df48abd2d88a50b454adf448a008703c04b
mabado <mathewabado@gmail.com>
2015-04-20 23:08:18 -0400

    Added a silly .c file, which calls main() recursively.

---
+++ recursive_main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int retval = 0;
+    if (argc != 0) {
+        printf("Why did you type %s?\n", argv[argc-1]);
+        retval = 1 + main(argc - 1, argv);
+    }
+    return retval;
+}
